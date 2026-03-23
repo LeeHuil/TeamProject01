acdcd98197f2cf481f178d71bd1954a043d65645
LAPTOP-TANPRLPA\huil3 <huil33@naver.com>
2026-03-23 15:46:58 +0900

    no message

--- Source/GitTest/MyActor.cpp
+++ Source/GitTest/MyActor.cpp
@@ -13,13 +13,21 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
+<<<<<<< HEAD
 	//UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+=======
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+>>>>>>> eec6702543bc168f58c8f9f259c202b4880f0a32
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+<<<<<<< HEAD
 	//UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+=======
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+>>>>>>> eec6702543bc168f58c8f9f259c202b4880f0a32
 }
 
